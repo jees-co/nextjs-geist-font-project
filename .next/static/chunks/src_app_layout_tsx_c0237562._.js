1d14893c5f8ef118a549184eb228bed5d3abc902
jees-co <jeesagency26@gmail.com>
2025-07-25 10:51:03 +0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1753415462875

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__8a9f255d._.css",
+  "static/chunks/node_modules_next_dist_c3a575e9._.js",
+  "static/chunks/src_components_Navigation_tsx_e839a609._.js"
+],
+    source: "dynamic"
+});
